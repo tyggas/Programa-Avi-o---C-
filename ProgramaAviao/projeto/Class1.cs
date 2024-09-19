7abe743db049563a02cc55cf2b7a777cf2d0d472
Tiago Grigull <tagrigull@gmail.com>
2024-09-18 21:35:43 -0300

    Add files via upload

---
+++ ProgramaAviao/projeto/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Sistemas
+{
+    public class Sistemas
+    {
+        private static int NUMERO_DE_LUGARES = 12;
+        private static bool[] lugaresOcupados = new bool[NUMERO_DE_LUGARES];
+
+        public static void FazerReserva()
+        {
+            Console.WriteLine($"Informe o número da poltrona que deseja reservar\n(De 1 a {NUMERO_DE_LUGARES}):\n");
+            int numeroPoltrona = int.Parse(Console.ReadLine());
+
+            if (numeroPoltrona < 1 || numeroPoltrona > NUMERO_DE_LUGARES)
+            {
+                Console.WriteLine("Número de poltrona inválido!\n");
+                return;
+            }
+            else if (lugaresOcupados[numeroPoltrona - 1])
+            {
+                Console.WriteLine("Esta poltrona já está ocupada\n");
+            }
+            else
+            {
+                lugaresOcupados[numeroPoltrona - 1] = true;
+                Console.WriteLine($"Poltrona {numeroPoltrona} reservada com sucesso!\n");
+            }
+        }
+
+        public static void ListarLugaresOcupados()
+        {
+            Console.WriteLine("Lugares ocupados:");
+            for (int i = 0; i < NUMERO_DE_LUGARES; i++)
+            {
+                if (lugaresOcupados[i])
+                {
+                    Console.WriteLine($"Poltrona {i + 1}\n");
+                }
+            }
+        }
+
+        public static void ListarLugaresLivres()
+        {
+            Console.WriteLine("Lugares livres:");
+            for (int i = 0; i < NUMERO_DE_LUGARES; i++)
+            {
+                if (!lugaresOcupados[i])
+                {
+                    Console.WriteLine($"Poltrona {i + 1}\n");
+                }
+            }
+        }
+
+        public static void MostrarPercentualOcupacao()
+        {
+            int lugaresOcupadosCount = 0;
+            foreach (bool ocupado in lugaresOcupados)
+            {
+                if (ocupado)
+                {
+                    lugaresOcupadosCount++;
+                }
+            }
+
+            double percentualOcupacao = (double)lugaresOcupadosCount / NUMERO_DE_LUGARES * 100;
+            Console.WriteLine($"Percentual de ocupação: {percentualOcupacao:F2}%\n");
+        }
+    }
+}
---
+++ ProgramaAviao/projeto/Program.cs
@@ -0,0 +1,54 @@
+﻿using Sistemas;  // Importe o namespace da classe Sistemas
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char opcao;
+
+        do
+        {
+            Console.WriteLine("Escolha uma opção");
+            Console.WriteLine("a) Fazer reserva da poltrona");
+            Console.WriteLine("b) Listar lugares ocupados");
+            Console.WriteLine("c) Listar lugares livres");
+            Console.WriteLine("d) Mostrar percentual de ocupação");
+            Console.WriteLine("e) Sair");
+            Console.WriteLine("\nOpção:");
+            opcao = Console.ReadLine()[0];
+
+            switch (opcao)
+            {
+                case 'a':
+                    Console.WriteLine("Fazer Reserva");
+                    Sistemas.Sistemas.FazerReserva();  // Chama o método FazerReserva da classe Sistemas
+                    break;
+
+                case 'b':
+                    Console.WriteLine("Listar lugares ocupados");
+                    Sistemas.Sistemas.ListarLugaresOcupados();  // Chama o método ListarLugaresOcupados
+                    break;
+
+                case 'c':
+                    Console.WriteLine("Listar lugares disponíveis");
+                    Sistemas.Sistemas.ListarLugaresLivres();  // Chama o método ListarLugaresLivres
+                    break;
+
+                case 'd':
+                    Console.WriteLine("Mostrar percentual de ocupação");
+                    Sistemas.Sistemas.MostrarPercentualOcupacao();  // Chama o método MostrarPercentualOcupacao
+                    break;
+
+                case 'e':
+                    Console.WriteLine("Saindo do programa...");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+
+        } while (opcao != 'e');
+    }
+}
